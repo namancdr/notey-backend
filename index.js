629b0bb661b9bcf6651beabb31cd5581725be909
Naman Choudhary <35829405+namancdr@users.noreply.github.com>
2024-01-13 23:45:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
  app.listen(port, () => {
      console.log(`Notey Backend is running on port http://localhost:${port}`)
  })
